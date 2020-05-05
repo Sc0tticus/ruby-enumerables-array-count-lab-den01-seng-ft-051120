d396e4ad92a5a8b444870e5e8e65d8a8efbd791c
Scott Singer <ssinger303@gmail.com>
2020-05-05 03:10:18 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,10 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+<<<<<<< HEAD
   array.count { |element| element.is_a? String }
+=======
+  array.count { |String| String > 0 }
+>>>>>>> 783de499f1ddcbe16bb1a47a7cfafaaa0fc9e653
 end
 
 def count_empty_strings(array)
